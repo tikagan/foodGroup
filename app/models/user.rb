17a6f98b4ad4e45dc7a260b8f9e0cf887775bc94
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-21 15:04:11 -0400

    model user defined

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+	def self.find_or_create_from_auth_hash(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.firstname = auth.info.firstname
+			user.lastname = auth.info.lastname
+			user.email = auth.info.email
+			user.picture = auth.info.image
+			user.save!
+		end
+	end
+end
