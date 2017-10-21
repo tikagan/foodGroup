878be5afe797143282c31f0a5136216da9616c74
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-21 15:05:26 -0400

    omni auth config file created, google credentials added

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "132992573288-a4vhkjatl20bs534rd86ndr43j1arn4h.apps.googleusercontent.com", "Zx5dj6BPPKamqk-B0K_bHnwu"
+end
