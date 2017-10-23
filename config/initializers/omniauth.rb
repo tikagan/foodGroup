63711e33f029948feab6bba278c0abb7c7961f16
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-23 14:50:53 -0400

    google and facebook login credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "132992573288-a4vhkjatl20bs534rd86ndr43j1arn4h.apps.googleusercontent.com", "Zx5dj6BPPKamqk-B0K_bHnwu"
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    client_options: {
+      callback_path: 'auth/facebook/callback'
+    }
 end
