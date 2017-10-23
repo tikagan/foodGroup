3033a313949e7a32a13f27f52bae8ed294f5b2f4
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-23 16:05:42 -0400

    google and fb both populate the name colums in the db now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    scope: 'public_profile,email',
+    info_fields: 'first_name,last_name,email',
     client_options: {
-      callback_path: 'auth/facebook/callback'
+      callback_path: 'auth/facebook/callback',
     }
 end
