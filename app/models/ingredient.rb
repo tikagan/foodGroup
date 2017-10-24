1b05e6bf606b25a96ffc2ddfd276e5d02fd0c6c9
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-24 12:36:15 -0400

    create function modified to not save ingredient to database if it already exists

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,15 +2,18 @@ class Ingredient < ApplicationRecord
   has_many :grocery_list
   has_many :pantry
 
-  validate_uniqueness_of :name
+  validates_uniqueness_of :name
 
   def create
-    ing = Ingredient.new
+    ing = Ingredient.new(ingredient_params)
+    if !ing
+      ing.save!
+    end
   end
 
   private
   def ingredient_params
-  params.require(:ingredient)
-end
+    params.require(:name)
+  end
 
 end
