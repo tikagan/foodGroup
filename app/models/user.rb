70425ea3c82a2565b2d50612bca0498d9772896f
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-24 10:58:12 -0400

    find_or_create_from_auth_hash function changed to accomodate seed file, still works with omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
 	has_one :pantry
 
 	def self.find_or_create_from_auth_hash(auth)
-		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-			user.provider = auth.provider
-			user.uid = auth.uid
-			user.firstname = auth.info.first_name
-			user.lastname = auth.info.last_name
-			user.email = auth.info.email
-			user.picture = auth.info.image
+		where(provider: auth[:provider], uid: auth[:uid]).first_or_initialize.tap do |user|
+			user.provider = auth[:provider]
+			user.uid = auth[:uid]
+			user.firstname = auth[:info][:first_name]
+			user.lastname = auth[:info][:last_name]
+			user.email = auth[:info][:email]
+			user.picture = auth[:info][:image]
 			user.save!
 		end
 	end
