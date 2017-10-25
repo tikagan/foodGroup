f732abcda687a99f6d8844ad433804d0fc604410
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-25 12:52:55 -0400

    create function and private params function, not quite finished, needs db column name added

--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -1,4 +1,15 @@
 class GroceryList < ApplicationRecord
   belongs_to :ingredient
   belongs_to :user
+
+  def create
+    grocery_list = GroceryList.new(grocery_list_params)
+
+  end
+
+  private
+  def grocery_list_params
+    params.require(:user_id, :ingredient_id).permit(:quantity, :unit)
+  end
+
 end
