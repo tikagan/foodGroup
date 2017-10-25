e7a9f75e534b66803b500de2cb344dfcdeff76e0
Ali Abji <aliabji@gmail.com>
2017-10-25 14:43:06 -0400

    added jQuery and Axios CDN links in head

--- app/javascript/pantry/components/Pantry.jsx
+++ app/javascript/pantry/components/Pantry.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Navbar from './Navbar.jsx'
 import { Route, BrowserRouter,Link } from 'react-router-dom'
-import search from './search.jsx'
+import Search from './search.jsx'
 
 class  Pantry extends Component {
 
