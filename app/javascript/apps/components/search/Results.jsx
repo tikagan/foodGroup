dbeb0cf37ffcf2dee527412da94cdeb8369e652c
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-30 16:49:10 -0400

    move ajax request to db for pantry info into search.jsx, made recipieName searchable

--- app/javascript/apps/components/search/Results.jsx
+++ app/javascript/apps/components/search/Results.jsx
@@ -4,39 +4,37 @@ import lunr from 'lunr'
 import ResultItem from './ResultItem'
 
 class Results extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sortedResults: []
+    }
+  }
 
   componentDidUpdate(){
     var results = this.props.searchResults
-    var userIngredients = []
     //ajax call that gets the current user's pantry, singularizes the ingredients, and puts them in an array
-    $.ajax({
-      url: '/api/pantry',
-      type: 'GET',
-      data: 'json',
-      success: function(res) {
-        res.result.forEach(function(ingObj) {
-            userIngredients.push(pluralize.singular(ingObj.name))
-        })
-        console.log('userIngredients: ', userIngredients)
-      }
-    });
-    console.log('results: ', results)
+
+    // tokenizes recipeName without using Lunr's really intensive tokenizing function
+    results = results.map(r => {
+      r.nameTokens = r.recipeName.split(' ');
+      return r
+    })
+    // creates index for lunr to search on results arrap
     var idx = lunr(function () {
-      this.field('recipieName')
+      this.field('nameTokens')
       this.field('ingredients')
-      this.ref('recipeName')
       results.forEach((recipe) => {
         this.add(recipe)
       })
     })
-    console.log('soup: ', idx.search('soup'))
-
-
+    // searches results for userIngredients
+    this.setState({sortedResults: idx.search(this.props.userIngredients)})
   };
 
   render(){
     // once componentDidMount sorts this.props.searchResults replace this with the newly sorted array
-      var resultItems = this.props.searchResults.map(function(result) {
+      var resultItems = this.state.sortedResults.map(function(result) {
       return <ResultItem key={result.id} img={result.smallImageUrls} link={result.id} name={result.recipeName} rating={result.rating} />
       });
       return(
