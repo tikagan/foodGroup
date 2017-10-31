f8c35302a0ec2b098daa3596147a08a0e858196b
Luke Siemens <lukesiemens@gmail.com>
2017-10-31 13:40:22 -0400

    added RecipeSearched component

---
+++ app/javascript/apps/recipiesearched/components/RecipeSearched.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from './Navbar.jsx'
+import { Route, BrowserRouter,Link } from 'react-router-dom'
+
+
+class  RecipeSearched extends Component {
+
+	constructor (props) {
+	    super(props)
+	    
+	    this.state = {
+	    	showCompnent: false.
+
+	    	
+	    };
+	    this._onButtonClick = this._onButtonClick.bind(this);
+	   
+	}
+	_onButtonClick() {
+		this.set.State({
+			showCompnent: !this.state.showCompnent
+		});
+	}
+
+		render() {
+		  return (
+		  	<div className="potluckBG">
+		  	  <Button onClick={this._onButtonClick}>Button</Button>
+        {this.state.showComponent ?
+         :
+           null
+        }
+			  <Navbar />
+			</div>
+			);
+		}
+	}
+
+export default RecipeSearched
