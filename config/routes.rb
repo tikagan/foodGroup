042228b7e7849977a5286f6a1d36e91fb59481ef
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-11-01 18:51:36 -0400

    spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   namespace :api do
     resource :pantry
     resources :potlucks
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
     resources :groceries
     resources :ingredients
     resources :grocery_ingredients
-    resources :recipies
+    resources :recipes
   end
 
   root to: "home#show"
