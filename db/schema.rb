0fc6cf4115e81346c1581d5291b9916dd8e939b7
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-11-01 16:47:55 -0400

    migration schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030225503) do
+ActiveRecord::Schema.define(version: 20171101204213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20171030225503) do
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
     t.string "image"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
