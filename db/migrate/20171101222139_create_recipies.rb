f518986233916207ec3108ee6181dc670a188394
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-11-01 18:49:33 -0400

    whoops

--- db/migrate/20171101222139_create_recipies.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecipies < ActiveRecord::Migration[5.1]
-  def change
-    create_table :recipies do |t|
-      t.string :name
-      t.string :description
-      t.string :URI
-    end
-  end
-end
--- db/migrate/20171101222832_create_potluck_recipies.rb
+++
@@ -1,8 +0,0 @@
-class CreatePotluckRecipies < ActiveRecord::Migration[5.1]
-  def change
-    create_table :potluck_recipies do |t|
-      t.references :potlucks, foreign_key: true
-      t.references :recipies, foreign_key: true
-    end
-  end
-end
