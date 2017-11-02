e4b91229b84e55b262bb7be3aa22b94232611d73
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-11-02 10:54:21 -0400

    posts to recipes database

--- app/javascript/apps/search/PotluckResultItem.jsx
+++ app/javascript/apps/search/PotluckResultItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class ResultItem extends Component {
+class PotluckResultItem extends Component {
   constructor(props) {
     super(props)
   }
@@ -22,20 +22,26 @@ class ResultItem extends Component {
 
   addToPotluck = (event) => {
     var URL = 'http://api.yummly.com/v1/api/recipe/' + this.props.link + '?_app_id=1187f4c6&_app_key=7dbff064930ce67f94b7ded79f8958f7'
+    var name = this.props.name
+    var image = this.props.img[0]
+    console.log('name: ', name)
+    console.log('image: ', image)
     axios.post('/api/recipes', {
-      params: {
-        name: this.props.recipeName,
-        image: this.props.img,
-        URI: URL
-      }
+        recipe: {
+          name: name,
+          image: image,
+          URI: URL
+        }
     })
-    .then (res)
-    console.log('res: ', res)
-    axios.post('/api/potluck_recipes', {
-      params: {
-        potluck_id: this.props.potluck_id,
-        recipe_id: this.props.id,
-      }
+    .then ((res) => {
+      console.log('res: ', res)
+      // var potluck_id = this.props.potluck_id
+      axios.post('/api/potluck_recipes', {
+        potluck_recipe: {
+          potluck_id: 1,
+          recipe_id: res.data.result.id,
+        }
+      })
     })
     .catch((err) => {
       console.log(err)
@@ -47,11 +53,11 @@ class ResultItem extends Component {
         <li className="recipe-search-item">
         <img className="search-item-image" src={this.props.img}></img>
         {this.props.name}, {this.props.rating} / 5
-        <button className= "btn btn-sm searchresult-button" type="submit"  onClick={this.addToPotluck}>Add to Potluck</button>
+        <button className= "btn btn-sm searchresult-button" type="submit"  onClick={null}>Add to Potluck</button>
         <button className= "btn btn-sm searchresult-button" type="submit"  onClick={this.recipeSearch}>Show Recipe</button>
         </li>
       );
   }
 };
 
-export default ResultItem
+export default PotluckResultItem
