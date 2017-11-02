77b0622f131b457eb3d39e85952bd11fd495daa7
Ali Abji <aliabji@gmail.com>
2017-11-02 11:43:55 -0400

    Update Gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -89,3 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+
