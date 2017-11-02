a06bcbdec5332071a3e8a1de1bd226d850e79368
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-11-01 20:37:43 -0400

    fixed tabbing

--- app/javascript/apps/search/ResultItem.jsx
+++ app/javascript/apps/search/ResultItem.jsx
@@ -1,31 +1,34 @@
 import React, { Component } from 'react'
 
 class ResultItem extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-      recipeSearch = (event) => {
-      var URL = 'http://api.yummly.com/v1/api/recipe/' + this.props.link + '?_app_id=1187f4c6&_app_key=7dbff064930ce67f94b7ded79f8958f7'
-        $.ajax({
-            type: "GET",
-            dataType: 'jsonp',
-            url: URL,
-            success: function(response){
-              window.open(response.attribution.url)
-            },
-            failure: function(data) {
-              console.log('failed')
-            }
-        });
-    };
+  recipeSearch = (event) => {
+  var URL = 'http://api.yummly.com/v1/api/recipe/' + this.props.link + '?_app_id=1187f4c6&_app_key=7dbff064930ce67f94b7ded79f8958f7'
+    $.ajax({
+        type: "GET",
+        dataType: 'jsonp',
+        url: URL,
+        success: function(response){
+          window.open(response.attribution.url)
+        },
+        failure: function(data) {
+          console.log('failed')
+        }
+    });
+  };
 
-    render(){
-        return (
-          <li className="recipe-search-item">
-          <img className="search-item-image" src={this.props.img}></img>
-          {this.props.name}, {this.props.rating} / 5
-          <button className= "btn btn-sm searchresult-button" type="submit"  onClick={this.recipeSearch}> Show Recipe</button>
-          </li>
-        );
-    }
+  render(){
+      return (
+        <li className="recipe-search-item">
+        <img className="search-item-image" src={this.props.img}></img>
+        {this.props.name}, {this.props.rating} / 5
+        <button className= "btn btn-sm searchresult-button" type="submit"  onClick={this.recipeSearch}>Show Recipe</button>
+        </li>
+      );
+  }
 };
 
 export default ResultItem
